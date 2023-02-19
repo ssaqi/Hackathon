baea3d345602ba1267d79b36b036ee023619fae8
Muhammad Saqib <89692631+ssaqi@users.noreply.github.com>
2023-02-19 15:57:13 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Details from './components/Details';
 import Errror from './components/Errror';
 import {Routes,Route} from "react-router-dom"
 import Setting from './Setting';
+import Admin from './components/Admin';
+import AdminMain from './components/AdminMain';
 
 function App() {
   return (
@@ -17,6 +19,8 @@ function App() {
       <Route path='/login' element={<Login />} />
       <Route path='/details' element={<Details />} />
       <Route path='/setting' element={<Setting/>} />
+      <Route path='/Admin' element={<Admin/>} />
+      <Route path='/AdminMain' element={<AdminMain/>} />
       <Route path='*' element={<Errror />} />
     </Routes>
   </>
